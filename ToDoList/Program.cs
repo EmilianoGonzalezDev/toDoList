7bb21e68461ca4db5a429dbf5f37baf482dd33e4
Emiliano Gonzalez <EMILIANO_G06@hotmail.com>
2025-06-29 02:28:36 -0300

    switch options moved to methods

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -23,57 +23,9 @@ while (menuOptionSelected != "0")
     switch (menuOptionSelected)
     {
         case "0": Environment.Exit(0); break;
-        case "1":
-            {
-                listTasks();
-                Console.WriteLine("Describe your new task: ");
-                var newTask = Console.ReadLine();
-                //todoList.Add(newTask ?? string.Empty);
-                if (newTask != "" && newTask != null)
-                {
-                    todoList.Add(newTask);
-                }
-                else
-                {
-                    Console.WriteLine("Please, write a name for the task.");
-                    Console.ReadKey(true);
-                }
-
-                Console.Clear();
-                break;
-            };
-        case "2":
-            {
-                listTasks();
-                Console.WriteLine("Task to delete: ");
-                var taskToDelete = Console.ReadLine();
-                int taskNumber = getTaskNumber(taskToDelete, todoList.Count);
-
-                if (taskNumber > 0)
-                {
-                    todoList.RemoveAt(taskNumber - 1);
-                }
-
-                Console.Clear();
-                break;
-            };
-        case "3":
-            {
-                listTasks();
-                Console.WriteLine("Task to update: ");
-                var taskToUpdate = Console.ReadLine();
-                int taskNumber = getTaskNumber(taskToUpdate, todoList.Count);
-
-                if (taskNumber > 0)
-                {
-                    Console.WriteLine("New task description: ");
-                    var newDescription = Console.ReadLine();
-                    todoList[taskNumber - 1] = newDescription ?? string.Empty;
-                }
-
-                Console.Clear();
-                break;
-            }
+        case "1": addTask(); break;
+        case "2": removeTask(); break;
+        case "3": updateTask(); break;
         case "4": break;
         default: Environment.Exit(0); break;
     }
@@ -117,3 +69,53 @@ int getTaskNumber(string? taskNumber, int listCount)
     }
 }
 
+void addTask()
+{
+    listTasks();
+    Console.WriteLine("Describe your new task: ");
+    var newTask = Console.ReadLine();
+
+    if (newTask != "" && newTask != null)
+    {
+        todoList.Add(newTask);
+    }
+    else
+    {
+        Console.WriteLine("Please, write a name for the task.");
+        Console.ReadKey(true);
+    }
+
+    Console.Clear();
+}
+
+void removeTask()
+{
+    listTasks();
+    Console.WriteLine("Task to delete: ");
+    var taskToDelete = Console.ReadLine();
+    int taskNumber = getTaskNumber(taskToDelete, todoList.Count);
+
+    if (taskNumber > 0)
+    {
+        todoList.RemoveAt(taskNumber - 1);
+    }
+
+    Console.Clear();
+}
+
+void updateTask()
+{
+    listTasks();
+    Console.WriteLine("Task to update: ");
+    var taskToUpdate = Console.ReadLine();
+    int taskNumber = getTaskNumber(taskToUpdate, todoList.Count);
+
+    if (taskNumber > 0)
+    {
+        Console.WriteLine("New task description: ");
+        var newDescription = Console.ReadLine();
+        todoList[taskNumber - 1] = newDescription ?? string.Empty;
+    }
+
+    Console.Clear();
+}
